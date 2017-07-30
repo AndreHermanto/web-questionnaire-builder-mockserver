fcfd630e22f7be5c0da64943bb1472b5b7b97e80
Craig McNamara <craig.mcnamara@genome.one>
2017-07-31 09:37:32 +1000

    add encryption and decryption

--- mockServer/index.js
+++ mockServer/index.js
@@ -1,42 +1,35 @@
 // server.js
-var jsonServer = require('json-server')
+var jsonServer = require('json-server');
 var schema = require('./db.js');
-var server = jsonServer.create()
-var router = jsonServer.router(schema())
-var middlewares = jsonServer.defaults()
+var server = jsonServer.create();
+var router = jsonServer.router(schema());
+var middlewares = jsonServer.defaults();
 
 // wrap results in data property
-router.render = function (req, res) {
+router.render = function(req, res) {
   res.jsonp({
-   data: res.locals.data
+    data: res.locals.data
+  });
+};
+
+server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    '/questionnaires/:questionnaireId/versions/:versionId':
+      '/versions/:versionId',
+    '/contexts/:contextId/preferences': '/contexts',
+    '/contexts/:contextId/preferences?key=:key': '/contexts',
+    '/contexts/:contextId/preferences/:id': '/contexts/:id',
+    '/responses/export': '/responses',
+    '/concepts/:conceptId': '/concepts',
+    '/prefix-search?prefix=:prefix': '/prefix-search'
   })
-}
+);
 
-server.use(middlewares)
+server.use(jsonServer.bodyParser);
 
-// add route for logging in
-server.post('/login', (req, res) => {
-  // username: test
-  // password: test
-  res.send('eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0Iiwicm9sZXMiOlsiZGVmYXVsdD1BRE1JTjs6OiJdLCJzdXBlclJvbGUiOiJVU0VSIiwiZXhwIjoxNDk1MDk4MjU3LCJpYXQiOjE0OTUwOTY0NTd9.bgpMrZ2IoMCvBFtp6-sAPlTh9lrMhHHZgDp0JWY7-MdAFnM-WISfE15K5TTiQGREQOTL5v7Lie6nPkc7PeqiEA');
-})
-
-server.get('/validate/timestamp/:timetamp', (req, res) => {
-  res.send(true);
-})
-
-server.use(jsonServer.rewriter({
-  '/questionnaires/:questionnaireId/versions/:versionId': '/versions/:versionId',
-  '/contexts/:contextId/preferences': '/contexts',
-  '/contexts/:contextId/preferences?key=:key': '/contexts',
-  '/contexts/:contextId/preferences/:id': '/contexts/:id',
-  '/responses/export' : '/responses',
-  '/concepts/:conceptId': '/concepts',
-  '/prefix-search?prefix=:prefix':'/prefix-search'
-}));
-
-server.use(jsonServer.bodyParser)
-server.use(function (req, res, next) {
+server.use(function(req, res, next) {
   console.log(req.body, res.body);
   if (req.method === 'POST' && req.url !== '/login') {
     req.body.dateCreated = Date.now();
@@ -44,12 +37,33 @@ server.use(function (req, res, next) {
     req.body.creator = 'John Smith';
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
+
+// validates and decrypts this info
+// it doesnt really, it just returns it, but the actual server version does
+server.post('/secure', (req, res) => {
+  const response = {
+    userId: req.body.userId,
+    consentTypeId: req.body.consentTypeId || 'fake-consent',
+    timestamp: req.body.timestamp || 'fake-timestamp',
+    jwt: 'fake-jwt'
+  };
+  res.send(response);
+});
+
+// add route for logging in
+server.post('/login', (req, res) => {
+  // username: test
+  // password: test
+  res.send(
+    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0Iiwicm9sZXMiOlsiZGVmYXVsdD1BRE1JTjs6OiJdLCJzdXBlclJvbGUiOiJVU0VSIiwiZXhwIjoxNDk1MDk4MjU3LCJpYXQiOjE0OTUwOTY0NTd9.bgpMrZ2IoMCvBFtp6-sAPlTh9lrMhHHZgDp0JWY7-MdAFnM-WISfE15K5TTiQGREQOTL5v7Lie6nPkc7PeqiEA'
+  );
+});
 
 server.use(router);
 
-server.listen(4000, function () {
+server.listen(4000, function() {
   console.log('***************************************');
   console.log('* Json-Server is running on port 4000 *');
   console.log('***************************************');
